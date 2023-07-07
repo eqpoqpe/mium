92f23665966a0ddda2876887d00a050b2a2a09b9
Ryan Martin <eqpoqpe@gmail.com>
2023-07-07 21:06:11 +0800

    upload

--- apps/cloud/src/main.ts
+++ apps/cloud/src/main.ts
@@ -20,5 +20,5 @@ register(app);
 const _ = app.listen(3000, "0.0.0.0", 0, () => {
   console.log(`
 🚀 Server ready at: http://localhost:3000
-⭐️ See sample requests: http://pris.ly/e/ts/rest-express#3-using-the-rest-api`)
+`)
 });
