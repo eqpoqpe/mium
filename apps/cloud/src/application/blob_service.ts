5077744acc64a43644219892ae5bc8150f4159a3
eqpoqpe <eqpoqpe@gmail.com>
2023-08-27 20:44:42 +0800

    upload

--- apps/cloud/src/application/blob_service.ts
+++ apps/cloud/src/application/blob_service.ts
@@ -1,11 +1,9 @@
 // Copyright (c) 2023 Ryan Martin
 // This code is licensed under MPL-2.0 license.
 
-import { RouterContext } from "@koa/router";
 import { } from "@prisma/client";
 
-// @ts-ignore
-async function blobService(ctx: RouterContext) {
+async function blobService() {
   return "";
 }
 
